caed6383b6c85950c4582718db6dc5620b9ab07f
2dam3 <2dam3@HZ114487.H012108.NET>
2020-12-01 14:49:18 +0100

    Feat: first commit

---
+++ distMix/script.js
@@ -0,0 +1,27 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/distMix/script"],{
+
+/***/ "./src/input.scss":
+/*!************************!*\
+  !*** ./src/input.scss ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvaW5wdXQuc2Nzcz82ZWIzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6Ii4vc3JjL2lucHV0LnNjc3MuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyByZW1vdmVkIGJ5IGV4dHJhY3QtdGV4dC13ZWJwYWNrLXBsdWdpbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/input.scss\n");
+
+/***/ }),
+
+/***/ 0:
+/*!**********************************************!*\
+  !*** multi ./src/script.js ./src/input.scss ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+!(function webpackMissingModule() { var e = new Error("Cannot find module 'C:\\Users\\2dam3.HZ114487\\Desktop\\Frontend\\LaravelMix\\src\\script.js'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+module.exports = __webpack_require__(/*! C:\Users\2dam3.HZ114487\Desktop\Frontend\LaravelMix\src\input.scss */"./src/input.scss");
+
+
+/***/ })
+
+},[[0,"/distMix/manifest"]]]);
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+const mix= require('laravel-mix'); 
+mix.sass('src/input.scss','distMix').js('src/index.js','distMix').sourceMaps()
+.copy('src/index.html','distMix').extract(['leaflet']);
